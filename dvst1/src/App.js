a437cbc8d0e1367ac5d78f23818d088901754453
Dessa Goodlett <53787065+dmhabh1992@users.noreply.github.com>
2020-02-03 17:40:58 -0600

    added nav

--- dvst1/src/App.js
+++ dvst1/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="navLinks">
+          {/* <Route to="/" component={home}></Route>
+          <Route to="resources" component={resources}></Route>
+          <Route to="about" component={about}></Route>
+          <Route to="register" component={register}></Route>
+          <Route to="login" component={login}></Route> */}
+        </div>
+        <img src="https://images.unsplash.com/photo-1469571486292-0ba58a3f068b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt=""/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a>
+
         </a>
       </header>
     </div>
   );
 }
+
+
 
 export default App;
